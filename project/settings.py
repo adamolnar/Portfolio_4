03e12c0573610c4f4e7e0f13112a826263a6dcdd
adamolnar <adamolnar121@gmail.com>
2023-11-04 21:10:20 +0000

    Add ALLOWED_HOSTS link to heroku

--- project/settings.py
+++ project/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [ '','8000-adamolnar-portfolio4-hygjwyuh38g.ws-eu105.gitpod.io']
+ALLOWED_HOSTS = ['portfolio4-4e23d6cdeeab.herokuapp.com','8000-adamolnar-portfolio4-hygjwyuh38g.ws-eu105.gitpod.io']
 
 
 # Application definition
